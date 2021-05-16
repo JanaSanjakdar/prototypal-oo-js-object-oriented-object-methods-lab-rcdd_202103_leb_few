290009cbb7910cecaabbf4633935df16067df27c
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-05-16 08:54:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function BoardMember (name, homeState, training){
 } 
 
 BoardMember.prototype.veto = function() {
-  return "No, I must disagree";
+  return 'No, I must disagree';
 };
 
-BoardMember.prototype.approve =function(){
+BoardMember.prototype.approve = function(){
   return 'You can do that!'
 };
 
-BoardMember.prototype.doCharity =function(){
+BoardMember.prototype.doCharity = function(){
   return 'I like to help people.'
 };
 
