4453ecaf2b094afb4410f5002193004d4e73cb22
jana sanjakdar <jana.sanjakdar@hotmail.com>
2021-05-16 08:49:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ BoardMember.prototype.veto = function() {
 BoardMember.prototype.approve =function(){
   return 'You can do that!'
 }
+
+BoardMember.prototype.approve =function(){
+  return 'You can do that!'
+}
